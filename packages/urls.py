85e440a90a478680e672759ecf4799df8a436d3a
bmich22 <barbmichel22@gmail.com>
2025-05-09 12:36:28 +0200

    Added package_detail view, add edit and delete package views, allow site admins to add/edit/delete from frontend

--- packages/urls.py
+++ packages/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.packages, name='packages'),
+    path('add/', views.add_package, name='add_package'),
+    path('edit/<int:package_id>/', views.edit_package, name='edit_package'),
+    path('delete/<int:package_id>/', views.delete_package, name='delete_package'),
 ]
--- packages/views.py
+++ packages/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Package
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib import messages
+from .forms import PackageForm
+
 
 # Create your views here.
 
@@ -11,5 +15,39 @@ def packages(request):
     return render(request, 'packages/packages.html', {'packages': packages})
 
 
+def package_detail(request, package_id):
+    package = get_object_or_404(Package, id=package_id)
+    return render(request, 'packages/package_detail.html', {'package': package})
+
+
+@staff_member_required
+def add_package(request):
+    form = PackageForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Package added successfully!")
+        return redirect('packages')
+    return render(request, 'packages/package_form.html', {'form': form, 'title': 'Add Package'})
+
+
+@staff_member_required
+def edit_package(request, package_id):
+    package = get_object_or_404(Package, id=package_id)
+    form = PackageForm(request.POST or None, instance=package)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Package updated successfully!")
+        return redirect('packages')
+    return render(request, 'packages/package_form.html', {'form': form, 'title': 'Edit Package'})
+
+
+@staff_member_required
+def delete_package(request, package_id):
+    package = get_object_or_404(Package, id=package_id)
+    if request.method == 'POST':
+        package.delete()
+        messages.success(request, f"'{package.name}' was deleted.")
+        return redirect('packages')
+    return render(request, 'packages/package_confirm_delete.html', {'package': package})
 
 
