b039bca01e3ac0105a80e8136c536f9da21cc7b0
bmich22 <barbmichel22@gmail.com>
2025-05-11 11:53:17 +0200

    Add purchase model to checkout app so profile can show purchase history, update user_profile model

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+from packages.models import Package
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='purchases')
+    package = models.ForeignKey(Package, on_delete=models.SET_NULL, null=True)
+    sessions_used = models.PositiveIntegerField(default=0)
+    purchased_on = models.DateTimeField(auto_now_add=True)
+    expires_on = models.DateTimeField(null=True, blank=True)
+
+    stripe_checkout_id = models.CharField(max_length=255, blank=True, null=True)
+    stripe_payment_intent = models.CharField(max_length=255, blank=True, null=True)
+    payment_status = models.CharField(max_length=50, default='unconfirmed')
+
+    def sessions_remaining(self):
+        if self.package:
+            return self.package.num_sessions - self.sessions_used
+        return 0
+
+    def __str__(self):
+        return f"{self.user.username} – {self.package.name if self.package else 'Unknown Package'}"
--- user_profiles/models.py
+++ user_profiles/models.py
@@ -19,15 +19,7 @@ class UserProfile(models.Model):
     last_name = models.CharField(max_length=50, blank=True)
     grade_year = models.CharField(max_length=20, blank=True)  # Example: "Year 11", "IB2"
     parent_email = models.EmailField(blank=True, null=True)
-    package = models.ForeignKey(Package, on_delete=models.SET_NULL, null=True, blank=True)
     subjects = models.ManyToManyField('Subject', blank=True)
-    package_assigned_date = models.DateTimeField(null=True, blank=True)
-    sessions_used = models.PositiveIntegerField(default=0)
-
-    def sessions_remaining(self):
-        if self.package:
-            return self.package.num_sessions - self.sessions_used
-        return 0
 
     def __str__(self):
         return self.user.username
