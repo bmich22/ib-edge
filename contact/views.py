2a9e5b235d0d54e7fd054f2630ccd760fbc279b3
bmich22 <barbmichel22@gmail.com>
2025-05-13 11:56:31 +0200

    Added contact template and contact success template

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
+from django.conf import settings
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def contact(request):
-    """ A view to return the reviews page """
-    
-    return render(request, 'contacts/contact.html')
+    """ A view to display and process the contact form with email notification """
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        contact = form.save()
+        send_mail(
+            subject="New Contact Message",
+            message=f"From {contact.name} <{contact.email}>:\n\n{contact.message}",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.CONTACT_EMAIL],
+        )
+        return redirect('contact_success')
+
+    return render(request, 'contacts/contact.html', {'form': form})
