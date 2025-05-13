9a658c14276f5851d87ff5aeccc26799537e87b6
bmich22 <barbmichel22@gmail.com>
2025-05-13 13:02:45 +0200

    Added contact to admin panel with is-read column

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ContactMessage
 
 # Register your models here.
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'timestamp', 'is_read')  # ✅ Show in list
+    list_filter = ('is_read',)
+    search_fields = ('name', 'email', 'message')
+    ordering = ('-timestamp',)
+    readonly_fields = ('name', 'email', 'message', 'timestamp')
+    actions = ['mark_as_read']
+
+    @admin.action(description='Mark selected messages as read')
+    def mark_as_read(self, request, queryset):
+        queryset.update(is_read=True)
