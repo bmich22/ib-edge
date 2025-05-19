cfc35a88819341922ba5f3746e75d0561db60a9f
bmich22 <barbmichel22@gmail.com>
2025-05-20 00:24:40 +0200

    Edited 400 to test

--- ib_edge/settings.py
+++ ib_edge/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "ib_edge.urls"
