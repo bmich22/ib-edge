156c6a87304e29a33da9a93c090d75c0100a5afc
bmich22 <barbmichel22@gmail.com>
2025-05-21 15:45:26 +0200

    Added more organized purchase model to admin panel

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import Purchase
 
-# Register your models here.
 
-admin.site.register(Purchase)
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'package',
+        'subject_choice',
+        'customer_email',
+        'purchased_on',
+        'expires_on',
+        'payment_status',
+    )
+    list_filter = (
+        'payment_status',
+        'package',
+        'subject_choice',
+        'purchased_on',
+    )
+    search_fields = (
+        'user__username',
+        'user__email',
+        'customer_email',
+        'stripe_payment_intent',
+        'stripe_checkout_id',
+    )
+    ordering = ('-purchased_on',)
